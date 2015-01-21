191ca28bdc466f1d3461e2cb4f23d16141ec0bd9
Jonathan Kemp <kempdogg@gmail.com>
2015-01-20 23:24:13 -0500

    Initial commit.

---
+++ packages/style-data/README.md
@@ -0,0 +1,15 @@
+# style-data
+
+> Get the content of style tags.
+
+## Install
+
+Install with [npm](https://npmjs.org/package/style-data)
+
+```
+npm install --save style-data
+```
+
+## License
+
+MIT © [Jonathan Kemp](http://jonkemp.com)
---
+++ packages/style-data/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var mediaQueryText = require('mediaquery-text'),
+    cheerio = require('cheerio');
+
+module.exports = function (html, options, callback) {
+    var results = {},
+        $ = cheerio.load(html),
+        styleDataList,
+        styleData;
+
+    results.css = [];
+
+    $('style').each(function (index, element) {
+        styleDataList = element.childNodes;
+        if (styleDataList.length !== 1) {
+            callback(new Error('empty style element'));
+            return;
+        }
+        styleData = styleDataList[0].data;
+        if (options.applyStyleTags) {
+            results.css.push(styleData);
+        }
+        if (options.removeStyleTags) {
+            if (options.preserveMediaQueries) {
+                var mediaQueries = mediaQueryText(element.childNodes[0].nodeValue);
+                element.childNodes[0].nodeValue = mediaQueries;
+            } else {
+                $(element).remove();
+            }
+        }
+
+        results.html = $.html();
+    });
+    callback(null, results);
+};
