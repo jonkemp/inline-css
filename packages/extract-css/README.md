d1811da6204140c8bd0c34b652593226bce5d371
Jonathan Kemp <kempdogg@gmail.com>
2015-01-20 23:38:15 -0500

    Initial commit.

---
+++ packages/extract-css/README.md
@@ -0,0 +1,15 @@
+# extract-css
+
+> Extract the CSS from an HTML document.
+
+## Install
+
+Install with [npm](https://npmjs.org/package/extract-css)
+
+```
+npm install --save extract-css
+```
+
+## License
+
+MIT © [Jonathan Kemp](http://jonkemp.com)
---
+++ packages/extract-css/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var Batch = require('batch'),
+    getStylesData = require('style-data'),
+    getStylesheetList = require('list-stylesheets'),
+    getHrefContent = require('href-content');
+
+module.exports = function (html, options, callback) {
+    var batch = new Batch();
+    var data = getStylesheetList(html, options);
+    batch.push(function (callback) {
+        getStylesData(data.html, options, callback);
+    });
+    data.hrefs.forEach(function (stylesheetHref) {
+        batch.push(function (callback) {
+            getHrefContent(stylesheetHref, options.url, callback);
+        });
+    });
+    batch.end(function (err, results) {
+        if (err) {
+            return callback(err);
+        }
+        var stylesData = results.shift();
+        results.forEach(function (content) {
+            stylesData.css.push(content);
+        });
+        var css = stylesData.css.join('\n');
+        callback(null, stylesData.html, css);
+    });
+};
