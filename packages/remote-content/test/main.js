a6187804e6216a8ddab62da57dd18512acffcaf3
Jonathan Kemp <kempdogg@gmail.com>
2015-02-01 16:51:36 -0500

    Update tests.

--- packages/remote-content/test/main.js
+++ packages/remote-content/test/main.js
@@ -6,21 +6,10 @@
 var should = require('should'),
     fs = require('fs'),
     path = require('path'),
-    gutil = require('gulp-util'),
     getRemoteContent = require('../index');
-
-function getFile(filePath) {
-    return new gutil.File({
-        path: path.resolve(filePath),
-        cwd: './test/',
-        base: path.dirname(filePath),
-        contents: new Buffer(String(fs.readFileSync(filePath)))
-    });
-}
 
 function compare(remotePath, fixturePath, done) {
     getRemoteContent(remotePath, function (err, css) {
-        console.log(css);
         css.should.be.equal(String(fs.readFileSync(fixturePath)));
 
         done();
