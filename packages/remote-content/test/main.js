d536be9304cab2e21eee2e951751541b6e2b9fd1
Jonathan Kemp <kempdogg@gmail.com>
2015-02-01 17:03:29 -0500

    Fix typo.

--- packages/remote-content/test/main.js
+++ packages/remote-content/test/main.js
@@ -16,7 +16,7 @@ function compare(remotePath, fixturePath, done) {
     });
 }
 
-describe('href-content', function() {
+describe('remote-content', function() {
     it('Should get remote content from link tags in an HTML document', function(done) {
         compare('https://raw.githubusercontent.com/jonkemp/remote-content/master/test/fixtures/file.css', path.join('test', 'fixtures', 'file.css'), done);
     });
