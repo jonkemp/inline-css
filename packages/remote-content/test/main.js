96f366de1dae20022edde053b97e669d8e1a5da5
Jonathan Kemp <kempdogg@gmail.com>
2015-04-10 22:49:01 -0400

    Increase test timeout.

--- packages/remote-content/test/main.js
+++ packages/remote-content/test/main.js
@@ -17,6 +17,8 @@ function compare(remotePath, fixturePath, done) {
 }
 
 describe('remote-content', function() {
+    this.timeout(5000);
+    
     it('Should get remote content from link tags in an HTML document', function(done) {
         compare('https://raw.githubusercontent.com/jonkemp/remote-content/master/test/fixtures/file.css', path.join('test', 'fixtures', 'file.css'), done);
     });
