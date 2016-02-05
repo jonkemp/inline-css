a2db0eef18a52d0d27875a477817fd546adc974c
Jonathan Kemp <kempdogg@gmail.com>
2016-02-05 14:29:05 +0000

    Fix eslint issues.

--- packages/remote-content/index.js
+++ packages/remote-content/index.js
@@ -5,11 +5,10 @@ var superagent = require('superagent');
 module.exports = function (remoteUrl, callback) {
     superagent.get(remoteUrl).buffer().end(function (err, resp) {
         if (err) {
-            callback(err);
+            return callback(err);
         } else if (resp.ok) {
-            callback(null, resp.text);
-        } else {
-            callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
+            return callback(null, resp.text);
         }
+        callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
     });
 };
