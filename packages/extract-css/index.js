e1be70a47495570b9cd58d89be770a6b75219055
Jonathan Kemp <kempdogg@gmail.com>
2016-02-15 17:32:18 +0000

    Fix eslint issues.

--- packages/extract-css/index.js
+++ packages/extract-css/index.js
@@ -7,28 +7,31 @@ var assert = require('assert'),
     getHrefContent = require('href-content');
 
 module.exports = function (html, options, callback) {
-    var batch = new Batch();
-    var data = getStylesheetList(html, options);
-    batch.push(function (callback) {
-        getStylesData(data.html, options, callback);
+    var batch = new Batch(),
+        data = getStylesheetList(html, options);
+
+    batch.push(function (cb) {
+        getStylesData(data.html, options, cb);
     });
     if (data.hrefs.length) {
         assert.ok(options.url, 'options.url is required');
     }
     data.hrefs.forEach(function (stylesheetHref) {
-        batch.push(function (callback) {
-            getHrefContent(stylesheetHref, options.url, callback);
+        batch.push(function (cb) {
+            getHrefContent(stylesheetHref, options.url, cb);
         });
     });
     batch.end(function (err, results) {
+        var stylesData = results.shift(),
+            css;
+
         if (err) {
             return callback(err);
         }
-        var stylesData = results.shift();
         results.forEach(function (content) {
             stylesData.css.push(content);
         });
-        var css = stylesData.css.join('\n');
+        css = stylesData.css.join('\n');
         callback(null, stylesData.html, css);
     });
 };
