59bd77108144d1bfb8d7533ee184b22e4f52774c
Jonathan Kemp <kempdogg@gmail.com>
2016-02-24 19:51:47 +0000

    Add support for passing options to cheerio.

--- packages/list-stylesheets/index.js
+++ packages/list-stylesheets/index.js
@@ -1,18 +1,24 @@
 'use strict';
 
 var cheerio = require('cheerio'),
-    extend = require('extend');
+    pick = require('object-pick');
 
 module.exports = function (html, options) {
-    var results = {};
-    var $ = cheerio.load(html, extend(options.cheerioOptions, {
-        decodeEntities: false
-    }));
+    var results = {},
+        $ = cheerio.load(html, pick(options, [
+            'xmlMode',
+            'decodeEntities',
+            'lowerCaseTags',
+            'lowerCaseAttributeNames',
+            'recognizeCDATA',
+            'recognizeSelfClosing'
+        ]));
 
     results.hrefs = [];
 
     $('link').each(function (index, element) {
         var $el = $(element);
+
         if ($el.attr('rel') && $el.attr('rel').toLowerCase() === 'stylesheet') {
             if (options.applyLinkTags) {
                 results.hrefs.push($el.attr('href'));
