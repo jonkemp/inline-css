89bd2d9649d09a442edeff551d6559ad2388441e
Jonathan Kemp <kempdogg@gmail.com>
2016-02-25 23:27:26 -0500

    Add malformed css test.

--- packages/extract-css/test/main.js
+++ packages/extract-css/test/main.js
@@ -41,4 +41,15 @@ describe('extract-css', function() {
         };
         compare(path.join('test', 'fixtures', 'in.html'), path.join('test', 'expected', 'out.html'), path.join('test', 'expected', 'file.css'), options, done);
     });
+
+    it('Should handle malformed CSS', function(done) {
+        var options = {
+            applyStyleTags: true,
+            removeStyleTags: true,
+            applyLinkTags: true,
+            removeLinkTags: true,
+            preserveMediaQueries: false
+        };
+        compare(path.join('test', 'fixtures', 'malformed.html'), path.join('test', 'expected', 'malformed.html'), path.join('test', 'expected', 'malformed.css'), options, done);
+    });
 });
