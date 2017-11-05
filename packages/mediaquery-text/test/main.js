ddf8b8339e070299886e37b5dc2c3b3f1695bd3f
Jonathan Kemp <kempdogg@gmail.com>
2017-11-04 22:45:22 -0400

    Add test for CSSFontFaceRule.

--- packages/mediaquery-text/test/main.js
+++ packages/mediaquery-text/test/main.js
@@ -9,8 +9,22 @@ var should = require('should'),
 
 describe('mediaquery-text', function() {
     it('Should return Media Query text for a CSS source', function(done) {
-        var mediaQueries = mediaQueryText(String(fs.readFileSync(path.join('test', 'fixtures', 'file.css'))));
-        mediaQueries.should.be.equal(String(fs.readFileSync(path.join('test', 'expected', 'file.css'))));
+        var mediaQueries = mediaQueryText(
+            String(fs.readFileSync(path.join('test', 'fixtures', 'file.css')))
+        );
+        mediaQueries.should.be.equal(
+            String(fs.readFileSync(path.join('test', 'expected', 'file.css')))
+        );
+        done();
+    });
+
+    it('Should return Media Query text for a CSS source with @font-face', function(done) {
+        var mediaQueries = mediaQueryText(
+            String(fs.readFileSync(path.join('test', 'fixtures', 'font-face.css')))
+        );
+        mediaQueries.should.be.equal(
+            String(fs.readFileSync(path.join('test', 'expected', 'font-face.css')))
+        );
         done();
     });
 });
