35f1405c34e1ecb294feb13d9aa4eaabbbbb1774
Jonathan Kemp <kempdogg@gmail.com>
2018-03-05 21:42:22 -0500

    Ignore code blocks during parsing.

--- packages/style-data/index.js
+++ packages/style-data/index.js
@@ -5,20 +5,61 @@ var mediaQueryText = require('mediaquery-text'),
     extend = require('extend'),
     pick = require('object.pick');
 
+function replaceCodeBlock(html, re, block) {
+    return html.replace(re, function () {
+        return block;
+    });
+}
+
 module.exports = function (html, options, callback) {
     var results = {},
-        $ = cheerio.load(html, extend({
-            decodeEntities: false
-        }, pick(options, [
-            'xmlMode',
-            'decodeEntities',
-            'lowerCaseTags',
-            'lowerCaseAttributeNames',
-            'recognizeCDATA',
-            'recognizeSelfClosing'
-        ]))),
-        styleDataList,
-        styleData;
+        codeBlocks = {
+            EJS: { start: '<%', end: '%>' },
+            HBS: { start: '{{', end: '}}' }
+        },
+        codeBlockLookup = [],
+        encodeCodeBlocks = function (_html) {
+            var __html = _html,
+                blocks = options.codeBlocks || codeBlocks;
+
+            Object.keys(blocks).forEach(function (key) {
+                var re = new RegExp(blocks[key].start + '([\\S\\s]*?)' + blocks[key].end, 'g');
+
+                __html = __html.replace(re, function (match) {
+                    codeBlockLookup.push(match);
+                    return 'EXCS_CODE_BLOCK_' + (codeBlockLookup.length - 1) + '_';
+                });
+            });
+            return __html;
+        },
+        decodeCodeBlocks = function (_html) {
+            var index, re,
+                __html = _html;
+
+            for (index = 0; index < codeBlockLookup.length; index++) {
+                re = new RegExp('EXCS_CODE_BLOCK_' + index + '_(="")?', 'gi');
+                __html = replaceCodeBlock(__html, re, codeBlockLookup[index]);
+            }
+            return __html;
+        },
+        encodeEntities = function (_html) {
+            return encodeCodeBlocks(_html);
+        },
+        decodeEntities = function (_html) {
+            return decodeCodeBlocks(_html);
+        },
+        $, styleDataList, styleData;
+
+    $ = cheerio.load(encodeEntities(html), extend({
+        decodeEntities: false
+    }, pick(options, [
+        'xmlMode',
+        'decodeEntities',
+        'lowerCaseTags',
+        'lowerCaseAttributeNames',
+        'recognizeCDATA',
+        'recognizeSelfClosing'
+    ])));
 
     results.css = [];
 
@@ -45,7 +86,7 @@ module.exports = function (html, options, callback) {
         }
     });
 
-    results.html = $.html();
+    results.html = decodeEntities($.html());
 
     callback(null, results);
 };
--- packages/style-data/test/main.js
+++ packages/style-data/test/main.js
@@ -1,5 +1,3 @@
-/*eslint-disable */
-
 'use strict';
 
 var should = require('should'),
@@ -27,13 +25,14 @@ function compare(fixturePath, expectedHTML, css, options, done) {
     });
 }
 
-describe('style-data', function() {
-    it('Should separate css and html', function(done) {
+describe('style-data', function () {
+    it('Should separate css and html', function (done) {
         var options = {
             applyStyleTags: true,
             removeStyleTags: true,
             preserveMediaQueries: false
         };
+
         compare(
             path.join('test', 'fixtures', 'in.html'),
             path.join('test', 'expected', 'out.html'),
@@ -43,12 +42,13 @@ describe('style-data', function() {
         );
     });
 
-    it('Should leave html from no style html', function(done) {
+    it('Should leave html from no style html', function (done) {
         var options = {
             applyStyleTags: true,
             removeStyleTags: true,
             preserveMediaQueries: false
         };
+
         compare(
             path.join('test', 'fixtures', 'no-style-tag', 'in.html'),
             path.join('test', 'expected', 'no-style-tag', 'out.html'),
@@ -58,16 +58,49 @@ describe('style-data', function() {
         );
     });
 
-    it('Should leave style blocks if they contain media queries', function(done) {
+    it('Should leave style blocks if they contain media queries', function (done) {
         var options = {
             applyStyleTags: true,
             removeStyleTags: true,
             preserveMediaQueries: true
         };
+
         compare(
             path.join('test', 'fixtures', 'media-queries', 'in.html'),
             path.join('test', 'expected', 'media-queries', 'out.html'),
             [ '\n      h1 {\n        border: 1px solid #ccc;\n      }\n    ', '\n      @media only screen and (min-width: 640px) {\n        .headline {\n          color: blue;\n        }\n      }\n    '],
+            options,
+            done
+        );
+    });
+
+    it('Should ignore hbs code blocks', function (done) {
+        var options = {
+            applyStyleTags: true,
+            removeStyleTags: true,
+            preserveMediaQueries: true
+        };
+
+        compare(
+            path.join('test', 'fixtures', 'codeblocks.html'),
+            path.join('test', 'expected', 'codeblocks.html'),
+            [],
+            options,
+            done
+        );
+    });
+
+    it('Should ignore ejs code blocks', function (done) {
+        var options = {
+            applyStyleTags: true,
+            removeStyleTags: true,
+            preserveMediaQueries: true
+        };
+
+        compare(
+            path.join('test', 'fixtures', 'ejs.html'),
+            path.join('test', 'expected', 'ejs.html'),
+            [],
             options,
             done
         );
