47bb9eeb05858d815bf398a4472717cad815fa36
markfalk <mfalk@dotloop.com>
2018-06-03 13:47:58 -0700

    Added proxy support (#2)
    * Added proxy support
    
    * Feedback implemented
    
    * Reverted version number to original base version

--- packages/remote-content/index.js
+++ packages/remote-content/index.js
@@ -1,14 +1,25 @@
 'use strict';
 
-var superagent = require('superagent');
+var superagent = require('superagent'),
+    getProxyForUrl = require('proxy-from-env').getProxyForUrl;
+
+// extend with Request#proxy()
+require('superagent-proxy')(superagent);
 
 module.exports = function (remoteUrl, callback) {
-    superagent.get(remoteUrl).buffer().end(function (err, resp) {
-        if (err) {
-            return callback(err);
-        } else if (resp.ok) {
-            return callback(null, resp.text);
-        }
-        callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
-    });
+    var proxyUrl = getProxyForUrl(remoteUrl),
+        superagentCallback = function (err, resp) {
+            if (err) {
+                return callback(err);
+            } else if (resp.ok) {
+                return callback(null, resp.text);
+            }
+            return callback(new Error('GET ' + remoteUrl + ' ' + resp.status));
+        };
+
+    if (proxyUrl) {
+        superagent.get(remoteUrl).proxy(proxyUrl).buffer().end(superagentCallback);
+    } else {
+        superagent.get(remoteUrl).buffer().end(superagentCallback);
+    }
 };
