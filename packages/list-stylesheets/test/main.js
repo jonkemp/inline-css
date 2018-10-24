f0c207ecd4d12d2d6a572474ae5f754de32cc5a2
Jonathan Kemp <kempdogg@gmail.com>
2018-10-23 23:21:29 -0400

    Update dependencies, remove gulp-util.

--- packages/list-stylesheets/test/main.js
+++ packages/list-stylesheets/test/main.js
@@ -6,11 +6,11 @@
 var should = require('should'),
     fs = require('fs'),
     path = require('path'),
-    gutil = require('gulp-util'),
+    Vinyl = require('vinyl'),
     getStylesheetList = require('../index');
 
 function getFile(filePath) {
-    return new gutil.File({
+    return new Vinyl({
         path: path.resolve(filePath),
         cwd: './test/',
         base: path.dirname(filePath),
