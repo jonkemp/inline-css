916101f2f32d97dd4cbd74ef3216ee2fc173e59e
Jonathan Kemp <kempdogg@gmail.com>
2018-10-27 20:47:34 -0400

    Update to gulp 4.0. (#5)

--- packages/mediaquery-text/gulpfile.js
+++ packages/mediaquery-text/gulpfile.js
@@ -1,25 +1,24 @@
 'use strict';
 var gulp = require('gulp'),
     eslint = require('gulp-eslint'),
-    mocha = require('gulp-mocha');
+    mocha = require('gulp-mocha'),
+    paths = {
+        scripts: [ './*.js', '!./gulpfile.js' ]
+    };
 
-var paths = {
-    scripts: ['./*.js', '!./gulpfile.js']
-};
-
-gulp.task('lint', function() {
+gulp.task('lint', function () {
     return gulp.src(paths.scripts)
         .pipe(eslint())
         .pipe(eslint.format());
 });
 
-gulp.task('test', function() {
+gulp.task('test', function () {
     return gulp.src('./test/*.js')
         .pipe(mocha());
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.scripts, ['lint', 'test']);
+    gulp.watch(paths.scripts, gulp.parallel('lint', 'test'));
 });
 
-gulp.task('default', ['lint', 'test', 'watch']);
+gulp.task('default', gulp.parallel('lint', 'test', 'watch'));
