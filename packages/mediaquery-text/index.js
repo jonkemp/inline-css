6246e00c652ead3645f99435df6e33a56913d923
Jonathan Kemp <kempdogg@gmail.com>
2020-01-19 09:57:42 -0500

    Update to es6+.
    Remove support for node 8.
    
    Update dependencies.
    
    Update eslint config.

--- packages/mediaquery-text/index.js
+++ packages/mediaquery-text/index.js
@@ -1,8 +1,6 @@
-'use strict';
-
-var cssom = require('cssom'),
-    os = require('os'),
-    CSSFontFaceRule = require('cssom/lib/CSSFontFaceRule').CSSFontFaceRule;
+const cssom = require('cssom');
+const os = require('os');
+const CSSFontFaceRule = require('cssom/lib/CSSFontFaceRule').CSSFontFaceRule;
 
 /**
  * Returns Media Query text for a CSS source.
@@ -11,18 +9,18 @@ var cssom = require('cssom'),
  * @api public
  */
 
-module.exports = function (css) {
-    var rules = cssom.parse(css).cssRules || [],
-        queries = [],
-        queryMedia,
-        queryString,
-        style,
-        property,
-        value,
-        important,
-        result;
+module.exports = css => {
+    const rules = cssom.parse(css).cssRules || [];
+    const queries = [];
+    let queryMedia;
+    let queryString;
+    let style;
+    let property;
+    let value;
+    let important;
+    let result;
 
-    rules.forEach(function (query) {
+    rules.forEach(({type, media, cssRules}) => {
         /* CSS types
 		  STYLE: 1,
 		  IMPORT: 3,
@@ -30,21 +28,21 @@ module.exports = function (css) {
 		  FONT_FACE: 5,
 		 */
 
-        if (query.type === cssom.CSSMediaRule.prototype.type) {
-            queryMedia = Array.prototype.slice.call(query.media).join(', ');
+        if (type === cssom.CSSMediaRule.prototype.type) {
+            queryMedia = Array.prototype.slice.call(media).join(', ');
             queryString = [];
 
-            queryString.push(os.EOL + '@media ' + queryMedia + ' {');
+            queryString.push(`${os.EOL}@media ${queryMedia} {`);
 
-            query.cssRules.forEach(function (rule) {
+            cssRules.forEach(rule => {
                 if (rule.type === cssom.CSSStyleRule.prototype.type || rule.type === CSSFontFaceRule.prototype.type) {
-                    queryString.push('  ' + (rule.type === cssom.CSSStyleRule.prototype.type ? rule.selectorText : '@font-face') + ' {');
+                    queryString.push(`  ${rule.type === cssom.CSSStyleRule.prototype.type ? rule.selectorText : '@font-face'} {`);
 
                     for (style = 0; style < rule.style.length; style++) {
                         property = rule.style[style];
                         value = rule.style[property];
                         important = rule.style._importants[property] ? ' !important' : '';
-                        queryString.push('    ' + property + ': ' + value + important + ';');
+                        queryString.push(`    ${property}: ${value}${important};`);
                     }
                     queryString.push('  }');
                 }
--- packages/mediaquery-text/test/main.js
+++ packages/mediaquery-text/test/main.js
@@ -1,15 +1,13 @@
 /*eslint-disable */
 
-'use strict';
+const should = require('should');
+const fs = require('fs');
+const path = require('path');
+const mediaQueryText = require('../index');
 
-var should = require('should'),
-    fs = require('fs'),
-    path = require('path'),
-    mediaQueryText = require('../index');
-
-describe('mediaquery-text', function() {
-    it('Should return Media Query text for a CSS source', function(done) {
-        var mediaQueries = mediaQueryText(
+describe('mediaquery-text', () => {
+    it('Should return Media Query text for a CSS source', done => {
+        const mediaQueries = mediaQueryText(
             String(fs.readFileSync(path.join('test', 'fixtures', 'file.css')))
         );
         mediaQueries.should.be.equal(
@@ -18,8 +16,8 @@ describe('mediaquery-text', function() {
         done();
     });
 
-    it('Should return Media Query text for a CSS source with @font-face', function(done) {
-        var mediaQueries = mediaQueryText(
+    it('Should return Media Query text for a CSS source with @font-face', done => {
+        const mediaQueries = mediaQueryText(
             String(fs.readFileSync(path.join('test', 'fixtures', 'font-face.css')))
         );
         mediaQueries.should.be.equal(
