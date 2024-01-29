7e64bbb1301bae102f4cbbd1f184edb0b5641614
Sergei Lutov <47363641+codeshake@users.noreply.github.com>
2024-01-29 04:36:19 +0300

    Migrate from superagent to axios (#127)

--- packages/remote-content/index.js
+++ packages/remote-content/index.js
@@ -1,24 +1,27 @@
-const superagent = require('superagent');
+const axios = require('axios');
 const getProxyForUrl = require('proxy-from-env').getProxyForUrl;
 
-// extend with Request#proxy()
-require('superagent-proxy')(superagent);
-
 module.exports = (remoteUrl, callback) => {
+    const axiosConfig = {};
     const proxyUrl = getProxyForUrl(remoteUrl);
 
-    const superagentCallback = (err, resp) => {
-        if (err) {
-            return callback(err);
-        } else if (resp.ok) {
-            return callback(null, resp.text);
-        }
-        return callback(new Error(`GET ${remoteUrl} ${resp.status}`));
-    };
-
     if (proxyUrl) {
-        superagent.get(remoteUrl).proxy(proxyUrl).buffer().end(superagentCallback);
-    } else {
-        superagent.get(remoteUrl).buffer().end(superagentCallback);
+        const proxyUrlData = new URL(proxyUrl);
+
+        axiosConfig.proxy = {
+            protocol: proxyUrlData.protocol.replace(':', ''),
+            host: proxyUrlData.hostname,
+            port: proxyUrlData.port,
+        };
     }
+
+    axios.get(remoteUrl, axiosConfig).then(response => {
+        if (response.statusText === 'OK') {
+            return callback(null, response.data);
+        }
+
+        return callback(new Error(`GET ${remoteUrl} ${response.status}`));
+    }).catch(error => {
+        callback(error);
+    });
 };
